d0c1672189d9735416d7cf4c52ba0dc8d0068b1d
Dan Vega <danvega@gmail.com>
2024-04-23 14:48:49 -0400

    Add User Demo - Adding Repository and initial set of users

--- src/main/java/dev/danvega/hellohtmx/_02/UserController.java
+++ src/main/java/dev/danvega/hellohtmx/_02/UserController.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,20 +15,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
-    private final Faker faker;
+    private final UserRepository userRepository;
 
-    public UserController(Faker faker) {
-        this.faker = faker;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @GetMapping("")
     public String list(Model model) {
+        model.addAttribute("users", userRepository.findAll());
         return "02/index";
     }
 
     @GetMapping("/addUserModal")
     public String addUser() {
         return "02/modal :: modal";
+    }
+
+    @GetMapping("/getFakeUser")
+    public String getFakeUser(Model model) {
+        var user = userRepository.createFakeUser();
+        model.addAttribute("user",user);
+        return "02/new-user-form :: frmNewUser";
+    }
+
+    @PostMapping("")
+    public String save(@RequestBody User user, Model model) {
+        // add new user
+        return "02/new-user-row :: row";
     }
 
 }
---
+++ src/main/java/dev/danvega/hellohtmx/_02/UserRepository.java
@@ -0,0 +1,37 @@
+package dev.danvega.hellohtmx._02;
+
+import com.github.javafaker.Faker;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(UserRepository.class);
+    private final List<User> users = new ArrayList<>();
+    private final Faker faker;
+
+    public UserRepository(Faker faker) {
+        this.faker = faker;
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User createFakeUser() {
+        return new User(faker.name().fullName(), faker.job().title(), faker.internet().emailAddress(), "User");
+    }
+
+    @PostConstruct
+    private void init() {
+        List<User> fakers = List.of(createFakeUser(), createFakeUser(), createFakeUser());
+        users.addAll(fakers);
+    }
+
+}
